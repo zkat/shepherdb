4f63e6fb92563c08ca1ca56734cb16453a76eb59
Kat Marchán <kzm@sykosomatic.org>
2009-06-12 15:30:10 -0500

    Tagging progress.

--- psheep.lisp
+++ psheep.lisp
@@ -8,8 +8,21 @@
 (defun make-database (db-name &key (address "localhost") (port couch::*couchdb-port*))
   (make-instance 'database :db db-name :couch (couch:make-couch address port)))
 
+(define-condition invalid-db-error () ())
+
+(defgeneric validate-database (db))
+(defmethod validate-database (db)
+  (error "~A is not a database" db))
+(defmethod validate-database ((db database))
+  (if (and (activep db))
+      t
+      (error "DB ~A did not pass validation." db)))
+
 (defclass persistent-sheep (sheeple:standard-sheep)
   ((persistent-properties :initform nil :accessor sheep-persistent-properties)))
+(defmethod initialize-instance :around ((sheep persistent-sheep))
+  (unless (validate-database *db*)
+    (call-next-method)))
 
 (defmethod sheeple:direct-property-value ((sheep persistent-sheep) property-name)
   (if (persistent-property-p sheep property-name)
