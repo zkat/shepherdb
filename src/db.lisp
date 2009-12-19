4a774a70d8669713e2c872d7f9b001de88606897
Kat Marchán <kzm@sykosomatic.org>
2009-12-19 00:45:15 -0600

    Reorganized and simplified request-related stuff.

--- src/db.lisp
+++ src/db.lisp
@@ -7,53 +7,46 @@
    (port 5984)
    (name nil)))
 
+(defmessage db->url (db)
+  (:reply ((db =database=))
+    (with-properties (host port name) db
+      (format nil "http://~A:~A/~A" host port name))))
+
+(define-condition couchdb-error ()
+  ((name :initarg :name :reader error-name)
+   (reason :initarg :reason :reader error-reason))
+  (:report (lambda (condition stream)
+             (format stream "Error: ~A, Reason: ~A"
+                     (error-name condition)
+                     (error-reason condition)))))
+
+(defun signal-couchdb-error (error-msg)
+  (error 'couchdb-error
+         :name (cdr (assoc :error error-msg))
+         :reason (cdr (assoc :reason error-msg))))
+
+(defmessage db-request (db &key)
+  (:reply ((db =database=) &key (uri "") (method :get) content)
+    (let ((db-reply (json:decode-json-from-string
+                     (http-request (format nil "~A/~A" (db->url db) uri
+                                           :method method :content content)))))
+      (if (assoc :error db-reply)
+          (signal-couchdb-error db-reply)
+          db-reply))))
+
 (defun connect-to-db (name &key (host "127.0.0.1") (port 5984) (prototype =database=))
-  (let ((db-reply (json:decode-json-from-string
-                   (http-request (format nil "http://~A:~A/~A" host port name)))))
-    (if (assoc :error db-reply)
-        (error "Error: ~A, Reason: ~A" (cdr (assoc :error db-reply)) (cdr (assoc :reason db-reply)))
-        (create prototype 'host host 'port port 'name name))))
+  (let ((db (create prototype 'host host 'port port 'name name)))
+    (db-request db)
+    db))
 
 (defun create-db (name &key (host "127.0.0.1") (port 5984) (prototype =database=))
-  (let ((db-reply (json:decode-json-from-string
-                   (http-request (format nil "http://~A:~A/~A" host port name)
-                                 :method :put))))
-    (if (assoc :error db-reply)
-        (error "Error: ~A, Reason: ~A" (cdr (assoc :error db-reply)) (cdr (assoc :reason db-reply)))
-        (create prototype 'host host 'port port 'name name))))
+  (let ((db (create prototype 'host host 'port port 'name name)))
+    (db-request db :method :put)
+    db))
 
-(defmessage get-document (db id)
-  (:reply ((db =database=) id)
-    (let ((db-reply (json:decode-json-from-string
-                     (http-request (format nil "http://~A:~A/~A/~A"
-                                           (host db) (port db) (name db) id)))))
-      (if (assoc :error db-reply)
-          (error "Error: ~A, Reason: ~A" (cdr (assoc :error db-reply)) (cdr (assoc :reason db-reply)))
-          db-reply))))
-
-(defmessage all-documents (db)
-  (:reply ((db =database=))
-    (let ((db-reply (json:decode-json-from-string
-                     (http-request (format nil "http://~A:~A/~A/_all_docs"
-                                           (host db) (port db) (name db))))))
-      (if (assoc :error db-reply)
-          (error "Error: ~A, Reason: ~A" (cdr (assoc :error db-reply)) (cdr (assoc :reason db-reply)))
-          db-reply))))
-
-(defmessage put-document (db doc)
-  (:reply ((db =database=) doc)
-    (let ((db-reply (json:decode-json-from-string
-                     (http-request (format nil "http://~A:~A/~A/~A"
-                                           (host db) (port db) (name db)
-                                           (id doc)
-                                           (revision doc))
-                                   :method :put
-                                   :content (json:encode-json-alist-to-string
-                                             (remove :_id (remove :id doc :key 'car) :key 'car))))))
-      (if (assoc :error db-reply)
-          (error "Error: ~A, Reason: ~A" (cdr (assoc :error db-reply)) (cdr (assoc :reason db-reply)))
-          db-reply))))
-
+;;;
+;;; Documents
+;;;
 (defun id (doc)
   (or (cdr (assoc :id doc))
       (cdr (assoc :_id doc))))
@@ -61,3 +54,17 @@
 (defun revision (doc)
   (or (cdr (assoc :_rev doc))
       (cdr (assoc :rev doc))))
+
+(defmessage get-document (db id)
+  (:reply ((db =database=) id)
+    (db-request db :uri id)))
+
+(defmessage all-documents (db)
+  (:reply ((db =database=))
+    (db-request db :uri "_all_docs")))
+
+(defmessage put-document (db doc)
+  (:reply ((db =database=) doc)
+    (db-request db :uri (id doc)
+                :content (json:encode-json-alist-to-string
+                          (remove :_id (remove :id doc :key 'car) :key 'car)))))
